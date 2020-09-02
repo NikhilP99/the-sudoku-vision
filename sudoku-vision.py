da097d0210ef686ca603a8674c9616cbcd1844a0
Nikhil Patel <nikhilpatel.indore@gmail.com>
2020-09-02 23:50:45 +0530

    added video and image imput

--- sudoku-vision.py
+++ sudoku-vision.py
@@ -1,28 +1,44 @@
+import argparse
 from cv2 import cv2
 
-video = cv2.VideoCapture(0)
+# setting up parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', type=str, default='', help='Input file (eg: testpic.jpg)')
+args = parser.parse_args()
 
-print(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-print(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+def video_mode():
+    video = cv2.VideoCapture(0)
 
-#width
-# video.set(3,1280)
-#height
-# video.set(4,720)
+    while(video.isOpened()):
+        ret, frame = video.read()
+        if ret == True:
+            gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-while(video.isOpened()):
-    ret, frame = video.read()
+            cv2.imshow("Video screen",gray)
 
-    if ret == True:
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-        cv2.imshow("Video screen",gray)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        else:
             break
 
-    else:
-        break
+    video.release()
+    cv2.destroyAllWindows()
 
-video.release()
-cv2.destroyAllWindows()
+
+def main():
+
+    # if the user has not specified a file to load, use the video input
+    if args.file == '':
+        video_mode()
+    else:
+        image = cv2.imread(args.file)
+        cv2.imshow("image",image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
