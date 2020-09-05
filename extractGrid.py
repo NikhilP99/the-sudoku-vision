517e5b523d1478ae303bf63cd128dc63d0271a9e
Nikhil Patel <nikhilpatel.indore@gmail.com>
2020-09-05 18:45:16 +0530

    found corners from contour

--- extractGrid.py
+++ extractGrid.py
@@ -1,16 +1,40 @@
 from cv2 import cv2
+import numpy as np
+
+# get corners of a contour
+# uses the convex hull
+def get_corners(contours, corner_amount=4, max_iter=200):
+
+    coefficient = 1
+    while max_iter > 0 and coefficient >= 0:
+        max_iter = max_iter - 1
+
+        epsilon = coefficient * cv2.arcLength(contours, True)
+
+        poly_approx = cv2.approxPolyDP(contours, epsilon, True)
+        hull = cv2.convexHull(poly_approx)
+        if len(hull) == corner_amount:
+            return hull
+        else:
+            if len(hull) > corner_amount:
+                coefficient += .01
+            else:
+                coefficient -= .01
+    return None
+
 
 def solve_and_print(image):
 
     # preprocessing
     gray_sudoku = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    # blurred_sudoku = cv2.GaussianBlur(gray_sudoku,(5,5),0)
-    thresh = cv2.adaptiveThreshold(gray_sudoku,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-    blurred = cv2.medianBlur(thresh,3)
+    blurred_sudoku = cv2.GaussianBlur(gray_sudoku,(7,7),0)
+    thresh = cv2.adaptiveThreshold(blurred_sudoku,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,2)
+    # blurred = cv2.medianBlur(thresh,3)
 
-    # find sudoku
-    contours,_ = cv2.findContours(cv2.bitwise_not(blurred),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    # find contours
+    contours,_ = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
+    # get the sudoku contour
     sudoku_area = 0
     sudoku_contour = contours[0]
     for cnt in contours:
@@ -24,9 +48,25 @@ def solve_and_print(image):
             sudoku_contour = cnt
 
     perimeter = cv2.arcLength(sudoku_contour,True)
-    approx = cv2.approxPolyDP(sudoku_contour,0.1*perimeter,True)
+    # define the accuracy here
+    epsilon = 0.05*perimeter
+    approx = cv2.approxPolyDP(sudoku_contour,epsilon,True)
 
-    if len(approx) == 4:
-        cv2.drawContours(image,[sudoku_contour],-1,(0,255,0),2)
+    # if it is not a sudoku board, just return the frame without doing anything
+    if len(approx) != 4:
+        return image
+
+    # Yayy! we found the sudoku grid
+    # Now let us find the corners
+
+    corners = get_corners(sudoku_contour)
+    if corners is None:
+        return image
+
+    corners = corners.reshape(4,2)
+    cv2.circle(image, (corners[0][0], corners[0][1]), 5, (255,0,0), thickness=3, lineType=8, shift=0)
+    cv2.circle(image, (corners[1][0], corners[1][1]), 5, (255,0,0), thickness=3, lineType=8, shift=0)
+    cv2.circle(image, (corners[2][0], corners[2][1]), 5, (255,0,0), thickness=3, lineType=8, shift=0)
+    cv2.circle(image, (corners[3][0], corners[3][1]), 5, (255,0,0), thickness=3, lineType=8, shift=0)
 
     return image
